93f4ad261cfdda2a998f7c058940351b164568d6
jy64 <jiyeonbag626@gmail.com>
2026-09-09 13:54:38 +0900

    해피함수

--- pp2-2026/lab/week2/happy.py
+++ pp2-2026/lab/week2/happy.py
@@ -23,7 +23,7 @@ def test_happy_birthday2():
 def test_happy_birthday3():
     say_happy_birthday(3.14159)
     say_happy_birthday(100)
-    say_happy_birthday([1, 2, 3])
+    say_happy_birthday([1,2,3])
 
 
 if __name__=="__main__":
